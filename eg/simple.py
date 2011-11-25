2274b4ced676df568238856af866f183438da626
franck cuny <franck.cuny@gmail.com>
2011-11-25 14:45:06 -0800

    add pep8 script and update sample

--- eg/simple.py
+++ eg/simple.py
@@ -7,7 +7,7 @@ client = Client()
 request = Request('GET', 'http://lumberjaph.net')
 response = client.request(request)
 
-if response.is_success():
+if response.is_success:
     print response.status
 else:
     print response
@@ -15,9 +15,9 @@ else:
 try:
     response = client.get('http://lumberjaph.net/foo/bar/baz')
 except HTTPException, e:
-    if e.is_redirect():
+    if e.is_redirect:
         print "on a une redirection"
-    elif e.is_client_error():
+    elif e.is_client_error:
         print e
     else:
         print "something else is going on"
