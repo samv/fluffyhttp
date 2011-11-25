28ecae46de031e55329e6e3ca81cf477c749a927
franck cuny <franck.cuny@gmail.com>
2011-11-25 15:23:49 -0800

    remove dots

--- fluffyhttp/__init__.py
+++ fluffyhttp/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['Request', 'Client', 'Response', 'HTTPException']
 
-from .client import Client
-from .request import Request
-from .response import Response
-from .exception import *
-from .url import Url
+from client import Client
+from request import Request
+from response import Response
+from exception import *
+from url import Url
