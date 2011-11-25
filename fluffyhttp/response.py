4a9ae0f14f36907636ae1a14195c8020588a5edf
franck cuny <franck.cuny@gmail.com>
2011-11-25 14:09:10 -0800

    some properties for the response object

--- fluffyhttp/response.py
+++ fluffyhttp/response.py
@@ -1,11 +1,24 @@
 class Response(object):
 
-    def __init__(self, status, headers, content):
+    def __init__(self, status, headers, content, reason):
         self.status = status
-        self.headers = headers
-        self.content = content
+        self._headers = headers
+        self._content = content
+        self.reason = reason
 
     def is_success(self):
         if self.status >= 200 and self.status <300:
             return True
         return False
+
+    @property
+    def content(self):
+        pass
+
+    @property
+    def header(self):
+        pass
+
+    @property
+    def headers(self):
+        pass
