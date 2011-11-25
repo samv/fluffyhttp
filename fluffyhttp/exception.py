a1a9fba8234531b6648bd6edfb69b8a442e5fe46
franck cuny <franck.cuny@gmail.com>
2011-11-25 14:08:36 -0800

    add helpers method, stringify object

--- fluffyhttp/exception.py
+++ fluffyhttp/exception.py
@@ -1,16 +1,27 @@
-def http_exception(status):
-    raise HTTPException(status, reason='foo', headers=[])
+def http_exception(resp):
+    raise HTTPException(resp.status, reason=resp.reason, headers=[])
 
 class HTTPException(Exception):
 
     def __init__(self, status, reason, headers):
         self.status = status
+        self.reason = reason
         pass
 
     def __str__(self):
-        pass
+        return "%i %s" % (self.status, self.reason)
 
     def is_redirect(self):
         if self.status >= 300 and self.status < 400:
+            return True
+        return False
+
+    def is_client_error(self):
+        if self.status >= 400 and self.status < 500:
+            return True
+        return False
+
+    def is_server_error(self):
+        if self.status >= 500 and self.status < 600:
             return True
         return False
