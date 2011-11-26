726f7fb4f35a529ea30c11e1bf83990b71894c38
franck cuny <franck.cuny@gmail.com>
2011-11-25 18:17:26 -0800

    upate tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,11 +7,11 @@ class TestClient(TestCase):
     def test_base(self):
         client = Client()
         self.assertTrue(client)
-        self.assertEqual(client.useragent, 'python-fluffyhttp')
-        client = Client(useragent='foo')
-        self.assertEqual(client.useragent, 'foo')
+        self.assertEqual(client.agent, 'python-fluffyhttp')
+        client = Client(agent='foo')
+        self.assertEqual(client.agent, 'foo')
 
     def test_headers(self):
         client = Client()
-        self.assertTrue(client._default_headers['Connection'])
-        self.assertEqual(client._default_headers['User-Agent'], 'python-fluffyhttp')
+        self.assertTrue(client.default_headers.get('Connection'))
+        self.assertEqual(client.default_headers.get('User-Agent'), 'python-fluffyhttp')
