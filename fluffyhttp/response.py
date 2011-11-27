7058bacc4c694c61ba0c56d7f21cd89f5fa9e4da
franck cuny <franck.cuny@gmail.com>
2011-11-27 13:38:16 -0800

    add is_redirect, header works, and add list of redirects

--- fluffyhttp/response.py
+++ fluffyhttp/response.py
@@ -8,6 +8,7 @@ class Response(object):
         self._headers = headers
         self._content = content
         self.reason = reason
+        self.redirects = list()
 
     @property
     def is_success(self):
@@ -16,12 +17,17 @@ class Response(object):
         return False
 
     @property
+    def is_redirect(self):
+        if self.status >= 300 and self.status < 400:
+            return True
+        return False
+
+    @property
     def content(self):
         return self._content
 
-    @property
-    def header(self):
-        pass
+    def header(self, name):
+        return self._headers.get(name)
 
     @property
     def headers(self):
