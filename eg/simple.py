08c81e69330be369de066896fb0a22db89bd7753
franck cuny <franck.cuny@gmail.com>
2011-12-12 22:10:39 -0800

    simple eg

--- eg/simple.py
+++ eg/simple.py
@@ -8,21 +8,12 @@ request = Request('GET', 'http://lumberjaph.net')
 response = client.request(request)
 
 if response.is_success:
-    print response.status
+    print "yeah, success!"
+    print "status: {status}".format(status=response.status)
+    print "message: {message}".format(message=response.message)
+    print "last modified in epoch: {last_modified}".format(last_modified=response.last_modified())
+    print "last modified in string: {last_modified}".format(last_modified=response.header('Last-Modified'))
+    if response.content_is_text:
+        print "and our content is text!"
 else:
-    print response
-
-#try:
-    #response = client.get('http://lumberjaph.net/foo/bar/baz')
-#except HTTPException, e:
-    #if e.is_redirect:
-        #print "on a une redirection"
-    #elif e.is_client_error:
-        #print e
-    #else:
-        #print "something else is going on"
-
-
-#res = client.head('http://lumberjaph.net/');
-#print res.status
-#print res.content
+    print "oups! {status_line}".format(status_line=response.status_line)
