bd8f6a2f4cdd9ff1d00460026d842105fde1b197
franck cuny <franck.cuny@gmail.com>
2011-12-12 21:57:57 -0800

    add time2str

--- fluffyhttp/date.py
+++ fluffyhttp/date.py
@@ -1,5 +1,6 @@
 import email.utils
 import time
+from datetime import datetime
 
 
 class Date:
@@ -8,3 +9,13 @@ class Date:
     def str2time(cls, date):
         t = int(time.mktime(email.utils.parsedate(date)))
         return t
+
+    @classmethod
+    def time2str(cls, dt):
+        if (isinstance(dt, datetime) is False):
+            raise Exception("date is not a datetime object")
+
+        weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt.weekday()]
+        month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+             "Oct", "Nov", "Dec"][dt.month - 1]
+        return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (weekday, dt.day, month, dt.year, dt.hour, dt.minute, dt.second)
